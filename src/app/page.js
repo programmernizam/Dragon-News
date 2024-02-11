f0d1247d379122a62e5cb5747165fa722be311c4
programmerNizam <mdnizam12109@gmail.com>
2024-02-11 22:13:24 +0600

    [Building] Home Page Grid Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
+import { Grid, Item } from "@mui/material";
+
 export default function HomePage() {
   return (
-    <div className="text-red-500">page</div>
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
