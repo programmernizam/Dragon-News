289dd1f1bf22b7cd3fc21b0774373935f26ee90c
programmerNizam <mdnizam12109@gmail.com>
2024-02-11 22:35:13 +0600

    [Done] Design Latest News component

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -1,5 +1,6 @@
 import topNews from '@/assets/top-news.png';
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import Grid from '@mui/material/Grid';
 import Image from "next/image";
 export default function LatestNews() {
     return (
@@ -10,10 +11,12 @@ export default function LatestNews() {
                         <Image src={topNews} alt="top news" width={800} />
                     </CardMedia>
                     <CardContent>
+                        <p className='inline-block
+                     bg-red-500 px-3 py-1 text-white mb-3'>Technology</p>
                         <Typography gutterBottom variant="h5" component="div">
                             BITCOIN DEVELOPER MAILING LIST MIGRATES
                         </Typography>
-                        <Typography gutterBottom>
+                        <Typography gutterBottom className='my-3'>
                             By Nizam - Jan 10 2024
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
@@ -23,6 +26,100 @@ export default function LatestNews() {
                     </CardContent>
                 </CardActionArea>
             </Card>
+            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className='mt-2'>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia>
+                                <Image src={topNews} alt="top news" width={800} />
+                            </CardMedia>
+                            <CardContent>
+                                <p className='inline-block
+                     bg-red-500 px-3 py-1 text-white mb-3'>Technology</p>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    BITCOIN DEVELOPER MAILING LIST MIGRATES
+                                </Typography>
+                                <Typography gutterBottom className='my-3'>
+                                    By Nizam - Jan 10 2024
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia>
+                                <Image src={topNews} alt="top news" width={800} />
+                            </CardMedia>
+                            <CardContent>
+                                <p className='inline-block
+                     bg-red-500 px-3 py-1 text-white mb-3'>Technology</p>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    BITCOIN DEVELOPER MAILING LIST MIGRATES
+                                </Typography>
+                                <Typography gutterBottom className='my-3'>
+                                    By Nizam - Jan 10 2024
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia>
+                                <Image src={topNews} alt="top news" width={800} />
+                            </CardMedia>
+                            <CardContent>
+                                <p className='inline-block
+                     bg-red-500 px-3 py-1 text-white mb-3'>Technology</p>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    BITCOIN DEVELOPER MAILING LIST MIGRATES
+                                </Typography>
+                                <Typography gutterBottom className='my-3'>
+                                    By Nizam - Jan 10 2024
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia>
+                                <Image src={topNews} alt="top news" width={800} />
+                            </CardMedia>
+                            <CardContent>
+                                <p className='inline-block
+                     bg-red-500 px-3 py-1 text-white mb-3'>Technology</p>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    BITCOIN DEVELOPER MAILING LIST MIGRATES
+                                </Typography>
+                                <Typography gutterBottom className='my-3'>
+                                    By Nizam - Jan 10 2024
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
